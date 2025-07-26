e4bd7512480f5912f4162e390d28004cc9c8185b
bartmarkiewicz <bartmark322@gmail.com>
2025-07-26 13:03:50 +0100

    Added request parsing

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,72 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	requestBytes, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	requestLines := strings.Split(string(requestBytes), "\r\n")
+	//for _, line := range requestLines {
+	requestLine, err := parseRequestLine(requestLines[0])
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	request := Request{
+		RequestLine: *requestLine,
+	}
+	return &request, nil
+	//}
+}
+
+func parseRequestLine(line string) (*RequestLine, error) {
+	splitWords := strings.Split(line, " ")
+	if !isAllCapitalLetters(splitWords[0]) {
+		return nil, fmt.Errorf("could not extract method from line: %s", line)
+	}
+
+	method := splitWords[0]
+	httpVersionString := splitWords[2]
+	splitHttpVersion := strings.Split(httpVersionString, "/")
+
+	if len(splitHttpVersion) != 2 || splitHttpVersion[1] != "1.1" || splitHttpVersion[0] != "HTTP" {
+		return nil, fmt.Errorf("request must be HTTP/1.1 got %s", httpVersionString)
+	}
+
+	requestTarget := splitWords[1]
+
+	requestLine := RequestLine{
+		HttpVersion:   splitHttpVersion[1],
+		RequestTarget: requestTarget,
+		Method:        method,
+	}
+	return &requestLine, nil
+}
+
+func isAllCapitalLetters(s string) bool {
+	for _, r := range s {
+		if r < 'A' || r > 'Z' {
+			return false
+		}
+	}
+	return true
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,1 +1,64 @@
 package request
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+)
+
+func TestRequestLineParse_GoodGet(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_GoodGetWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_BadLine(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestRequestLineParse_GoodPost(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("POST / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_GoodPostWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("POST /fish?hahaDoesThisWork=True HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/fish?hahaDoesThisWork=True", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_BadPost(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("/coffee POST HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestRequestLineParse_WrongHttpVersion(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("PATCH /fish?hahaDoesThisWork=True HTTP/12.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestRequestLineParse_NoHttpVersion(t *testing.T) {
+	_, err := RequestFromReader(strings.NewReader("POST /fish?hahaDoesThisWork=True \r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
